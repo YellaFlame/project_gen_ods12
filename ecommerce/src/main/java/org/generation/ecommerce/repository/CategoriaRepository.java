cc89582ff8f3983079437328bfdc1a3775a3968f
hanely <hanely.menezes@gmail.com>
2021-07-02 17:26:44 -0300

    alteração com bug

--- ecommerce/src/main/java/org/generation/ecommerce/repository/CategoriaRepository.java
+++ ecommerce/src/main/java/org/generation/ecommerce/repository/CategoriaRepository.java
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Repository;
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	
 	public Optional<Categoria>  findByResiduoContainingIgnoreCase(String residuo);
-
+	asdf
 }
